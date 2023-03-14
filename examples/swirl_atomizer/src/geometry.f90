334e8b5819cdb6a531d1dfe2f0c27653eef8701a
jvg723 <jvg36@cornell.edu>
2023-03-14 11:53:09 -0400

    add mpi groups to swirl atomizer

--- examples/swirl_atomizer/src/geometry.f90
+++ examples/swirl_atomizer/src/geometry.f90
@@ -1,8 +1,9 @@
 !> Various definitions and tools for initializing NGA2 config
 module geometry
-   use config_class, only: config
+   use mpi_f08,        only: MPI_Group
+   use config_class,   only: config
    use ibconfig_class, only: ibconfig
-   use precision,    only: WP
+   use precision,      only: WP
    implicit none
    private
    
@@ -12,10 +13,19 @@ module geometry
    !> Block 2 (b2) -> Swirl atomizer, two-phase wtih VOF
    type(config),   target, public :: cfg2
 
+   !> Public declarations
+   public :: geometry_init,Din,Dout
+   public :: group1,isInGrp1
+   public :: group2,isInGrp2
+
    !> Pipe diameter 
    real(WP) :: Dout,Din
 
-   public :: geometry_init,Din,Dout
+   !> Two groups and partitions, along with logicals
+   integer, dimension(3) :: partition1,partition2
+   logical :: isInGrp1,isInGrp2
+   type(MPI_Group) :: group1,group2
+   
 
 contains
    
@@ -24,101 +34,119 @@ contains
    subroutine geometry_init
       use sgrid_class, only: sgrid
       use param,       only: param_read
+      use parallel,    only: comm,group,nproc,rank
+      use mpi_f08,     only: MPI_Group,MPI_Group_range_incl
       implicit none
       type(sgrid) :: grid
       
-      ! Create config for block 1
-      create_block1: block
-         use sgrid_class, only: cartesian
-         use parallel, only: group
-         use ibconfig_class, only: bigot,sharp
-         integer :: i,j,k,nx,ny,nz,no
-         real(WP) :: Lx,Ly,Lz,dx,radius
-         real(WP), dimension(:), allocatable :: x,y,z
-         integer, dimension(3) :: partition
-         ! Read in grid and geometry definition
-         call param_read('1 Pipe length',Lx)
-         call param_read('1 Outer Pipe diameter',Dout)
-         call param_read('1 Inner Pipe diameter',Din)
-         call param_read('1 ny',ny); allocate(y(ny+1))
-         call param_read('1 nx',nx); allocate(x(nx+1))
-         call param_read('1 nz',nz); allocate(z(nz+1))
-         ! Domain lengths
-         dx=Lx/real(nx,WP)
-         no=6
-         if (ny.gt.1) then
-            Ly=Dout+real(2*no,WP)*Dout/real(ny-2*no,WP)
-         else
-            Ly=dx
-         end if
-         if (nz.gt.1) then
-            Lz=Dout+real(2*no,WP)*Dout/real(ny-2*no,WP)
-         else
-            Lz=dx
-         end if
-         ! Create simple rectilinear grid
-         do i=1,nx+1
-            x(i)=real(i-1,WP)/real(nx,WP)*Lx-0.5_WP*Lx
-         end do
-         do j=1,ny+1
-            y(j)=real(j-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
-         end do
-         do k=1,nz+1
-            z(k)=real(k-1,WP)/real(nz,WP)*Lz-0.5_WP*Lz
-         end do
-         ! General serial grid object
-         grid=sgrid(coord=cartesian,no=2,x=x,y=y,z=z,xper=.true.,yper=.true.,zper=.true.,name='annular_pipe')
+      mpi_groups: block
+         integer, dimension(3,1) :: grange
+         integer :: ierr
          ! Read in partition
-         call param_read('1 Partition',partition,short='p')
-         ! Create partitioned grid
-         cfg1=ibconfig(grp=group,decomp=partition,grid=grid)
-         ! Create IB walls for this config
-         !> Create IB field
-         do k=cfg1%kmino_,cfg1%kmaxo_
-            do j=cfg1%jmino_,cfg1%jmaxo_
-               do i=cfg1%imino_,cfg1%imaxo_
-                  radius=sqrt(cfg1%ym(j)**2+cfg1%zm(k)**2)
-                  cfg1%Gib(i,j,k)=max(0.5_WP*Din-radius,radius-0.5_WP*Dout)
-               end do
+         call param_read('1 Partition',partition1,short='p')
+         call param_read('2 Partition',partition2,short='p')
+         ! Create an MPI group along with logical for the first grid on the lowest ranks
+         grange(:,1)=[0,product(partition1)-1,1]
+         call MPI_Group_range_incl(group,1,grange,group1,ierr)
+         isInGrp1=.false.; if (rank.le.product(partition1)-1) isInGrp1=.true.
+         ! Create an MPI group along with logical for the second grid on the highest ranks
+         grange(:,1)=[nproc-product(partition2),nproc-1,1]
+         call MPI_Group_range_incl(group,1,grange,group2,ierr)
+         isInGrp2=.false.; if (rank.ge.nproc-product(partition2)) isInGrp2=.true.
+      end block mpi_groups
+
+      ! Initialize grid 1
+      if (isInGrp1) then
+         ! Create config for block 1
+         create_block1: block
+            use sgrid_class, only: cartesian
+            use parallel, only: group
+            use ibconfig_class, only: bigot,sharp
+            integer :: i,j,k,nx,ny,nz,no
+            real(WP) :: Lx,Ly,Lz,dx,radius
+            real(WP), dimension(:), allocatable :: x,y,z               
+            ! Read in grid and geometry definition
+            call param_read('1 Pipe length',Lx)
+            call param_read('1 Outer Pipe diameter',Dout)
+            call param_read('1 Inner Pipe diameter',Din)
+            call param_read('1 ny',ny); allocate(y(ny+1))
+            call param_read('1 nx',nx); allocate(x(nx+1))
+            call param_read('1 nz',nz); allocate(z(nz+1))          
+            ! Domain lengths
+            dx=Lx/real(nx,WP)
+            no=6
+            if (ny.gt.1) then
+               Ly=Dout+real(2*no,WP)*Dout/real(ny-2*no,WP)
+            else
+               Ly=dx
+            end if
+            if (nz.gt.1) then
+               Lz=Dout+real(2*no,WP)*Dout/real(ny-2*no,WP)
+            else
+               Lz=dx
+            end if            
+            ! Create simple rectilinear grid
+            do i=1,nx+1
+               x(i)=real(i-1,WP)/real(nx,WP)*Lx-0.5_WP*Lx
             end do
-         end do
-         !> Get normal vector
-         call cfg1%calculate_normal()
-         !> Get VF field
-         call cfg1%calculate_vf(method=sharp,allow_zero_vf=.false.)
-      end block create_block1
+            do j=1,ny+1
+               y(j)=real(j-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
+            end do
+            do k=1,nz+1
+               z(k)=real(k-1,WP)/real(nz,WP)*Lz-0.5_WP*Lz
+            end do         
+            ! General serial grid object
+            grid=sgrid(coord=cartesian,no=2,x=x,y=y,z=z,xper=.true.,yper=.true.,zper=.true.,name='annular_pipe')          
+            ! Create partitioned grid
+            cfg1=ibconfig(grp=group1,decomp=partition1,grid=grid)         
+            ! Create IB walls for this config
+            !> Create IB field
+            do k=cfg1%kmino_,cfg1%kmaxo_
+               do j=cfg1%jmino_,cfg1%jmaxo_
+                  do i=cfg1%imino_,cfg1%imaxo_
+                     radius=sqrt(cfg1%ym(j)**2+cfg1%zm(k)**2)
+                     cfg1%Gib(i,j,k)=max(0.5_WP*Din-radius,radius-0.5_WP*Dout)
+                  end do
+               end do
+            end do           
+            !> Get normal vector
+            call cfg1%calculate_normal()           
+            !> Get VF field
+            call cfg1%calculate_vf(method=sharp,allow_zero_vf=.false.)
+         end block create_block1
+      end if
       
-      ! Create config for block 2
-      create_block2: block
-         use sgrid_class, only: cartesian
-         use parallel, only: group
-         integer :: i,j,k,nx,ny,nz
-         real(WP) :: Lx,Ly,Lz
-         real(WP), dimension(:), allocatable :: x,y,z
-         integer, dimension(3) :: partition
-         ! Read in grid and geometry definition
-         call param_read('2 Lx',Lx); call param_read('2 nx',nx); allocate(x(nx+1))
-         call param_read('2 Ly',Ly); call param_read('2 ny',ny); allocate(y(ny+1))
-         call param_read('2 Lz',Lz); call param_read('2 nz',nz); allocate(z(nz+1))
-         ! Create simple rectilinear grid
-         do i=1,nx+1
-            x(i)=real(i-1,WP)/real(nx,WP)*Lx
-         end do
-         do j=1,ny+1
-            y(j)=real(j-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
-         end do
-         do k=1,nz+1
-            z(k)=real(k-1,WP)/real(nz,WP)*Lz-0.5_WP*Lz
-         end do
-         ! General serial grid object
-         grid=sgrid(coord=cartesian,no=3,x=x,y=y,z=z,xper=.false.,yper=.true.,zper=.true.,name='swirl_atomizer')
-         ! Read in partition
-         call param_read('2 Partition',partition,short='p')
-         ! Create partitioned grid
-         cfg2=config(grp=group,decomp=partition,grid=grid)
-         ! Create masks for this config
-         cfg2%VF=1.0_WP
-      end block create_block2
+      ! Initialize grid 2
+      if (isInGrp2) then
+         ! Create config for block 2
+         create_block2: block
+            use sgrid_class, only: cartesian
+            use parallel, only: group
+            integer :: i,j,k,nx,ny,nz
+            real(WP) :: Lx,Ly,Lz
+            real(WP), dimension(:), allocatable :: x,y,z
+            ! Read in grid and geometry definition
+            call param_read('2 Lx',Lx); call param_read('2 nx',nx); allocate(x(nx+1))
+            call param_read('2 Ly',Ly); call param_read('2 ny',ny); allocate(y(ny+1))
+            call param_read('2 Lz',Lz); call param_read('2 nz',nz); allocate(z(nz+1))
+            ! Create simple rectilinear grid
+            do i=1,nx+1
+               x(i)=real(i-1,WP)/real(nx,WP)*Lx
+            end do
+            do j=1,ny+1
+               y(j)=real(j-1,WP)/real(ny,WP)*Ly-0.5_WP*Ly
+            end do
+            do k=1,nz+1
+               z(k)=real(k-1,WP)/real(nz,WP)*Lz-0.5_WP*Lz
+            end do
+            ! General serial grid object
+            grid=sgrid(coord=cartesian,no=3,x=x,y=y,z=z,xper=.false.,yper=.true.,zper=.true.,name='swirl_atomizer')
+            ! Create partitioned grid
+            cfg2=config(grp=group2,decomp=partition2,grid=grid)
+            ! Create masks for this config
+            cfg2%VF=1.0_WP
+         end block create_block2
+      end if
    
 
    end subroutine geometry_init
--- examples/swirl_atomizer/src/simulation.f90
+++ examples/swirl_atomizer/src/simulation.f90
@@ -2,6 +2,8 @@
 module simulation
    use string,            only: str_medium
    use precision,         only: WP
+   use geometry,          only: cfg1,group1,isInGrp1
+   use geometry,          only: cfg2,group2,isInGrp2
    use block1_class,      only: block1
    use block2_class,      only: block2
    use coupler_class,     only: coupler
@@ -30,29 +32,54 @@ contains
    
    !> Initialization of problem solver
    subroutine simulation_init
-      use geometry, only: cfg1,cfg2
       use param,    only: param_read
       implicit none
       
-      ! Initialize both blocks
-      b1%cfg=>cfg1; call b1%init()
-      b2%cfg=>cfg2; call b2%init()
+      ! Initialize both blocks from mpi groups
+      if (isInGrp1) then 
+         b1%cfg=>cfg1
+         call b1%init()
+         ! Time for annular pipe to become turbulent 
+         call param_read('Transistion',transition_time)
+      end if
+      if (isInGrp2) then 
+         b2%cfg=>cfg2
+         call b2%init()
+      end if
 
       ! Initialize the couplers
       coupler_prep: block
          use parallel, only: group
-         ! Block 1 to block 2
-         cpl12x=coupler(src_grp=group,dst_grp=group,name='pipe_to_swirl_x'); call cpl12x%set_src(cfg1,'x'); call cpl12x%set_dst(cfg2,'x'); call cpl12x%initialize()
-         cpl12y=coupler(src_grp=group,dst_grp=group,name='pipe_to_swirl_y'); call cpl12y%set_src(cfg1,'y'); call cpl12y%set_dst(cfg2,'y'); call cpl12y%initialize()
-         cpl12z=coupler(src_grp=group,dst_grp=group,name='pipe_to_swirl_z'); call cpl12z%set_src(cfg1,'z'); call cpl12z%set_dst(cfg2,'z'); call cpl12z%initialize()
-         allocate(U1on2(cfg2%imino_:cfg2%imaxo_,cfg2%jmino_:cfg2%jmaxo_,cfg2%kmino_:cfg2%kmaxo_)); U1on2=0.0_WP
-         allocate(V1on2(cfg2%imino_:cfg2%imaxo_,cfg2%jmino_:cfg2%jmaxo_,cfg2%kmino_:cfg2%kmaxo_)); V1on2=0.0_WP
-         allocate(W1on2(cfg2%imino_:cfg2%imaxo_,cfg2%jmino_:cfg2%jmaxo_,cfg2%kmino_:cfg2%kmaxo_)); W1on2=0.0_WP
+         ! Both groups prepare the coupler
+         if (isInGrp1.or.isInGrp2) then
+            ! Block 1 to block 2
+            cpl12x=coupler(src_grp=group1,dst_grp=group2,name='pipe_to_swirl_x') 
+            cpl12y=coupler(src_grp=group1,dst_grp=group2,name='pipe_to_swirl_y') 
+            cpl12z=coupler(src_grp=group1,dst_grp=group2,name='pipe_to_swirl_z') 
+            ! Set the grids
+            if (isInGrp1) then
+               call cpl12x%set_src(cfg1,'x')  
+               call cpl12y%set_src(cfg1,'y')  
+               call cpl12z%set_src(cfg1,'z')
+            end if  
+            if (isInGrp2) then 
+               call cpl12x%set_dst(cfg2,'x')
+               call cpl12y%set_dst(cfg2,'y')
+               call cpl12z%set_dst(cfg2,'z')
+            end if
+            ! Initialize the metrics
+            call cpl12x%initialize()
+            call cpl12y%initialize()
+            call cpl12z%initialize()
+            ! Allocate arrays for velocity coupling
+            if (isInGrp2) then
+               allocate(U1on2(cfg2%imino_:cfg2%imaxo_,cfg2%jmino_:cfg2%jmaxo_,cfg2%kmino_:cfg2%kmaxo_)); U1on2=0.0_WP
+               allocate(V1on2(cfg2%imino_:cfg2%imaxo_,cfg2%jmino_:cfg2%jmaxo_,cfg2%kmino_:cfg2%kmaxo_)); V1on2=0.0_WP
+               allocate(W1on2(cfg2%imino_:cfg2%imaxo_,cfg2%jmino_:cfg2%jmaxo_,cfg2%kmino_:cfg2%kmaxo_)); W1on2=0.0_WP
+            end if
+         end if
       end block coupler_prep
 
-      ! Time for annular pipe to become turbulent 
-      call param_read('Transistion',transition_time)
-      
    end subroutine simulation_init
    
    
@@ -62,41 +89,64 @@ contains
       integer :: i,j,k
 
       ! Advance block 1 until annular pipe is turbulent
-      do while (b1%time%t.le.transition_time)
+      if (isInGrp1) then 
+         
+         do while (b1%time%t.le.transition_time)
             
-         call b1%step()
+            call b1%step()
+         
+         end do
+         
+         ! Reset block 1 time to 0
+         b1%time%t=0.0_WP
 
-      end do
-
-      ! Reset block 1 time to 0
-      b1%time%t=0.0_WP
-
-      ! Exchange velocity field using cpl12x/y/z couplers  for the initial inflow velocity in block 2
-      U1on2=0.0_WP; call cpl12x%push(b1%fs%U); call cpl12x%transfer(); call cpl12x%pull(U1on2)
-      V1on2=0.0_WP; call cpl12y%push(b1%fs%V); call cpl12y%transfer(); call cpl12y%pull(V1on2)
-      W1on2=0.0_WP; call cpl12z%push(b1%fs%W); call cpl12z%transfer(); call cpl12z%pull(W1on2)
+      end if
       
       ! Perform time integration - block 2 is the main driver here
-      do while (.not.b2%time%done())
+      if (isInGrp1.or.isInGrp2) then
          
-         ! Advance block 2
-         call b2%step(U1on2,V1on2,W1on2)
+         do while (.not.b2%time%done())
+            
+            ! Exchange velocity field using cpl12x/y/z couplers
+            coupling_step: block
+               ! Zero the fields
+               U1on2=0.0_WP; V1on2=0.0_WP; W1on2=0.0_WP 
+               ! Push the data from B1
+               if (isInGrp1) then
+                  call cpl12x%push(b1%fs%U) 
+                  call cpl12y%push(b1%fs%V) 
+                  call cpl12z%push(b1%fs%W) 
+               end if
+               ! Transfer the data
+               call cpl12x%transfer() 
+               call cpl12y%transfer() 
+               call cpl12z%transfer()
+               ! Pull the data to B2
+               if (isInGrp2) then
+                  call cpl12x%pull(U1on2)
+                  call cpl12y%pull(V1on2)
+                  call cpl12z%pull(W1on2)
+               end if
+            end block coupling_step
+            
+            ! Advance block 2
+            if (isInGrp2) call b2%step(U1on2,V1on2,W1on2)
 
-         ! Advance block 1 until we've caught up
-         do while (b1%time%t.lt.b2%time%t)
+            ! Advance block 1 until we've caught up
+            if (isInGrp1) then
+               
+               do while (b1%time%t.lt.b2%time%t)
 
-            ! Advance block 1
-            call b1%step()
+                  ! Advance block 1
+                  call b1%step()
 
-            ! Exchange data using cpl12x/y/z couplers and the most recent velocity
-            U1on2=0.0_WP; call cpl12x%push(b1%fs%U); call cpl12x%transfer(); call cpl12x%pull(U1on2)
-            V1on2=0.0_WP; call cpl12y%push(b1%fs%V); call cpl12y%transfer(); call cpl12y%pull(V1on2)
-            W1on2=0.0_WP; call cpl12z%push(b1%fs%W); call cpl12z%transfer(); call cpl12z%pull(W1on2)
+               end do
 
+            end if
+   
          end do
- 
-      end do
-      
+
+      end if
       
    end subroutine simulation_run
       
@@ -106,8 +156,8 @@ contains
       implicit none
       
       ! Deallocate work arrays for both blocks
-      call b1%final()
-      call b2%final()
+      if (isInGrp1) call b1%final()
+      if (isInGrp2) call b2%final()
       
    end subroutine simulation_final
    
