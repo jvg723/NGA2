968655da42f0df9919c52ab179d17d4391d46654
jvg723 <jvg36@cornell.edu>
2023-04-16 09:57:33 -0400

    Revert trC calc

--- src/non_newtonian/fene_class.f90
+++ src/non_newtonian/fene_class.f90
@@ -101,21 +101,21 @@ contains
       allocate(f_C(this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_)); f_C=0.0_WP
 
       ! Trace of conformation tensor and ensure trC<Lmax^2
-      ! this%trC=this%SC(:,:,:,1)+this%SC(:,:,:,4)+this%SC(:,:,:,6)
-      do k=this%cfg%kmin_,this%cfg%kmax_
-         do j=this%cfg%jmin_,this%cfg%jmax_
-            do i=this%cfg%imin_,this%cfg%imax_
-               this%trC(i,j,k)=this%SC(i,j,k,1)+this%SC(i,j,k,4)+this%SC(i,j,k,6)
-               ! this%trC(i,j,k)=min(this%trC(i,j,k),(Lmax**2)-1.00_WP)
-               ! this%trC(i,j,k)=max(this%trC(i,j,k),3.0_WP)
-               if (this%trC(i,j,k).ge.Lmax**2) then
-                  this%trC(i,j,k)=(Lmax**2)-1.00_WP
-               elseif (this%trC(i,j,k).lt.3.0_WP) then
-                  this%trC(i,j,k)=3.00_WP
-               endif
-            end do
-         end do
-      end do 
+      this%trC=this%SC(:,:,:,1)+this%SC(:,:,:,4)+this%SC(:,:,:,6)
+      ! do k=this%cfg%kmin_,this%cfg%kmax_
+      !    do j=this%cfg%jmin_,this%cfg%jmax_
+      !       do i=this%cfg%imin_,this%cfg%imax_
+      !          this%trC(i,j,k)=this%SC(i,j,k,1)+this%SC(i,j,k,4)+this%SC(i,j,k,6)
+      !          ! this%trC(i,j,k)=min(this%trC(i,j,k),(Lmax**2)-1.00_WP)
+      !          ! this%trC(i,j,k)=max(this%trC(i,j,k),3.0_WP)
+      !          ! if (this%trC(i,j,k).ge.Lmax**2) then
+      !          !    this%trC(i,j,k)=(Lmax**2)-1.00_WP
+      !          ! elseif (this%trC(i,j,k).lt.3.0_WP) then
+      !          !    this%trC(i,j,k)=3.00_WP
+      !          ! endif
+      !       end do
+      !    end do
+      ! end do 
 
       select case (this%model)
          case (FENEP)
