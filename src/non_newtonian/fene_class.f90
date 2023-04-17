f89e4b0eecb8913eee49b340a3bca81866c4cfd5
jvg723 <jvg36@cornell.edu>
2023-04-17 12:20:11 -0400

    Alter trC calc

--- src/non_newtonian/fene_class.f90
+++ src/non_newtonian/fene_class.f90
@@ -102,20 +102,6 @@ contains
 
       ! Trace of conformation tensor and ensure trC<Lmax^2
       this%trC=this%SC(:,:,:,1)+this%SC(:,:,:,4)+this%SC(:,:,:,6)
-      ! do k=this%cfg%kmin_,this%cfg%kmax_
-      !    do j=this%cfg%jmin_,this%cfg%jmax_
-      !       do i=this%cfg%imin_,this%cfg%imax_
-      !          this%trC(i,j,k)=this%SC(i,j,k,1)+this%SC(i,j,k,4)+this%SC(i,j,k,6)
-      !          ! this%trC(i,j,k)=min(this%trC(i,j,k),(Lmax**2)-1.00_WP)
-      !          ! this%trC(i,j,k)=max(this%trC(i,j,k),3.0_WP)
-      !          ! if (this%trC(i,j,k).ge.Lmax**2) then
-      !          !    this%trC(i,j,k)=(Lmax**2)-1.00_WP
-      !          ! elseif (this%trC(i,j,k).lt.3.0_WP) then
-      !          !    this%trC(i,j,k)=3.00_WP
-      !          ! endif
-      !       end do
-      !    end do
-      ! end do 
 
       select case (this%model)
          case (FENEP)
