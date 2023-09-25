2b0f6b7bca92d6698dccdf4cd5a84c71b43b4b8c
jvg723 <jvg36@cornell.edu>
2023-09-25 09:06:51 -0400

    Remove impletened SR formation

--- src/non_newtonian/fene_class.f90
+++ src/non_newtonian/fene_class.f90
@@ -129,35 +129,13 @@ contains
       end do
    end subroutine get_CgradU
 
-   !> Add D*C terms to multiscalar residual from ePTT equation
-   subroutine get_affine(this,gradU,resSC)
+   !> Get D*C terms for PTT equation
+   subroutine get_affine(this,SR,resSC)
       implicit none
       class(fene), intent(inout) :: this
-      real(WP), dimension(1:,1:,this%cfg%imino_:,this%cfg%jmino_:,this%cfg%kmino_:), intent(in) :: gradU
+      real(WP), dimension(1:,this%cfg%imino_:,this%cfg%jmino_:,this%cfg%kmino_:), intent(in) :: SR
       real(WP), dimension(this%cfg%imino_:,this%cfg%jmino_:,this%cfg%kmino_:,1:), intent(inout) :: resSC
       integer :: i,j,k
-      real(WP), dimension(:,:,:,:), allocatable :: D
-
-      ! Allocate rate of deformation tensor components
-	   allocate(D(this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_,6))
-      
-      ! Rate of deformation tensor D=(1/2)(gradU+gradU^T)
-      do k=this%cfg%kmino_,this%cfg%kmaxo_
-         do j=this%cfg%jmino_,this%cfg%jmaxo_
-            do i=this%cfg%imino_,this%cfg%imaxo_
-               ! Skip non-solved cells
-               if (this%mask(i,j,k).ne.0) cycle
-               D(i,j,k,1)=gradU(1,1,i,j,k)
-               D(i,j,k,2)=0.5_WP*(gradU(2,1,i,j,k)+gradU(1,2,i,j,k))
-               D(i,j,k,3)=0.5_WP*(gradU(3,1,i,j,k)+gradU(1,3,i,j,k))
-               D(i,j,k,4)=gradU(2,2,i,j,k)
-               D(i,j,k,5)=0.5_WP*(gradU(3,2,i,j,k)+gradU(2,3,i,j,k))
-               D(i,j,k,6)=gradU(3,3,i,j,k)
-            end do
-         end do
-      end do
-
-      ! Get affinecoeff*(D*C+C*D) to residual
       resSC=0.0_WP
       do k=this%cfg%kmino_,this%cfg%kmaxo_
          do j=this%cfg%jmino_,this%cfg%jmaxo_
@@ -165,27 +143,23 @@ contains
                ! Skip non-solved cells
                if (this%mask(i,j,k).ne.0) cycle
                ! xx tensor component
-               resSC(i,j,k,1)=this%affinecoeff*2.0_WP*(this%SC(i,j,k,1)*D(i,j,k,1)+this%SC(i,j,k,2)*D(i,j,k,2)+this%SC(i,j,k,3)*D(i,j,k,3))
+               resSC(i,j,k,1)=-this%affinecoeff*2.0_WP*(SR(1,i,j,k)*this%SC(i,j,k,1)+SR(4,i,j,k)*this%SC(i,j,k,2)+SR(6,i,j,k)*this%SC(i,j,k,3))
                ! xy tensor component
-               resSC(i,j,k,2)=this%affinecoeff*((this%SC(i,j,k,1)*D(i,j,k,2)+this%SC(i,j,k,2)*D(i,j,k,4)+this%SC(i,j,k,3)*D(i,j,k,5))+&
-               &                                (this%SC(i,j,k,2)*D(i,j,k,1)+this%SC(i,j,k,4)*D(i,j,k,2)+this%SC(i,j,k,5)*D(i,j,k,3)))
+               resSC(i,j,k,2)=-this%affinecoeff*((SR(4,i,j,k)*this%SC(i,j,k,1)+SR(2,i,j,k)*this%SC(i,j,k,2)+SR(5,i,j,k)*this%SC(i,j,k,3))+&
+               &                                 (SR(1,i,j,k)*this%SC(i,j,k,2)+SR(4,i,j,k)*this%SC(i,j,k,4)+SR(6,i,j,k)*this%SC(i,j,k,5)))
                ! xz tensor component
-               resSC(i,j,k,3)=this%affinecoeff*((this%SC(i,j,k,1)*D(i,j,k,3)+this%SC(i,j,k,2)*D(i,j,k,5)+this%SC(i,j,k,3)*D(i,j,k,6))+&
-               &                                (this%SC(i,j,k,3)*D(i,j,k,1)+this%SC(i,j,k,5)*D(i,j,k,2)+this%SC(i,j,k,6)*D(i,j,k,3)))
+               resSC(i,j,k,3)=-this%affinecoeff*((SR(6,i,j,k)*this%SC(i,j,k,1)+SR(5,i,j,k)*this%SC(i,j,k,2)+SR(3,i,j,k)*this%SC(i,j,k,3))+&
+               &                                 (SR(1,i,j,k)*this%SC(i,j,k,3)+SR(4,i,j,k)*this%SC(i,j,k,5)+SR(6,i,j,k)*this%SC(i,j,k,6)))
                ! yy tensor component
-               resSC(i,j,k,4)=this%affinecoeff*2.0_WP*(this%SC(i,j,k,2)*D(i,j,k,5)+this%SC(i,j,k,4)*D(i,j,k,4)+this%SC(i,j,k,5)*D(i,j,k,5))
+               resSC(i,j,k,4)=-this%affinecoeff*2.0_WP*(SR(4,i,j,k)*this%SC(i,j,k,2)+SR(2,i,j,k)*this%SC(i,j,k,4)+SR(5,i,j,k)*this%SC(i,j,k,5))
                ! yz tensor component
-               resSC(i,j,k,5)=this%affinecoeff*((this%SC(i,j,k,2)*D(i,j,k,3)+this%SC(i,j,k,4)*D(i,j,k,5)+this%SC(i,j,k,5)*D(i,j,k,6))+&
-               &                                (this%SC(i,j,k,3)*D(i,j,k,2)+this%SC(i,j,k,5)*D(i,j,k,4)+this%SC(i,j,k,6)*D(i,j,k,5)))
+               resSC(i,j,k,5)=-this%affinecoeff*((SR(6,i,j,k)*this%SC(i,j,k,2)+SR(5,i,j,k)*this%SC(i,j,k,4)+SR(3,i,j,k)*this%SC(i,j,k,5))+&
+               &                                 (SR(4,i,j,k)*this%SC(i,j,k,3)+SR(2,i,j,k)*this%SC(i,j,k,5)+SR(5,i,j,k)*this%SC(i,j,k,6)))
                ! zz tensor component
-               resSC(i,j,k,6)=this%affinecoeff*2.0_WP*(this%SC(i,j,k,3)*D(i,j,k,3)+this%SC(i,j,k,5)*D(i,j,k,5)+this%SC(i,j,k,6)*D(i,j,k,6))
+               resSC(i,j,k,6)=-this%affinecoeff*2.0_WP*(SR(6,i,j,k)*this%SC(i,j,k,3)+SR(5,i,j,k)*this%SC(i,j,k,5)+SR(3,i,j,k)*this%SC(i,j,k,6))
             end do
          end do
       end do
-      
-      ! Deallocate rate of deformation tensor storage
-	   deallocate(D)   
-
    end subroutine get_affine
    
 
@@ -280,13 +254,13 @@ contains
             do j=this%cfg%jmino_,this%cfg%jmaxo_
                do i=this%cfg%imino_,this%cfg%imaxo_
                   if (this%mask(i,j,k).ne.0) cycle                              !< Skip non-solved cells
-                  coeff=1.00_WP+(this%elongvisc/(1.0_WP-this%affinecoeff)*((this%SC(i,j,k,1)+this%SC(i,j,k,4)+this%SC(i,j,k,6))-3.0_WP))
+                  coeff=1.00_WP+(this%elongvisc/(1.0_WP-this%affinecoeff))*((this%SC(i,j,k,1)+this%SC(i,j,k,4)+this%SC(i,j,k,6))-3.0_WP)
                   coeff=coeff/this%trelax                   !< Divide by relaxation time scale
                   resSC(i,j,k,1)=-coeff*(this%SC(i,j,k,1)-1.0_WP) !> xx tensor component
-                  resSC(i,j,k,2)=-coeff* this%SC(i,j,k,2)         !> xy tensor component
-                  resSC(i,j,k,3)=-coeff* this%SC(i,j,k,3)         !> xz tensor component
+                  resSC(i,j,k,2)=-coeff*(this%SC(i,j,k,2)-0.0_WP) !> xy tensor component
+                  resSC(i,j,k,3)=-coeff*(this%SC(i,j,k,3)-0.0_WP) !> xz tensor component
                   resSC(i,j,k,4)=-coeff*(this%SC(i,j,k,4)-1.0_WP) !> yy tensor component
-                  resSC(i,j,k,5)=-coeff* this%SC(i,j,k,5)         !> yz tensor component
+                  resSC(i,j,k,5)=-coeff*(this%SC(i,j,k,5)-0.0_WP) !> yz tensor component
                   resSC(i,j,k,6)=-coeff*(this%SC(i,j,k,6)-1.0_WP) !> zz tensor component
                end do
             end do
