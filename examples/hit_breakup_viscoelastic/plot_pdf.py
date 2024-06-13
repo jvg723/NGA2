e125da733dca796d76801fcdb2702adc2340a0da
jvg723 <jvg36@cornell.edu>
2024-06-13 14:36:31 -0400

    updates to compare viscosity ratio

--- examples/hit_breakup_viscoelastic/plot_pdf.py
+++ examples/hit_breakup_viscoelastic/plot_pdf.py
@@ -1,7 +1,7 @@
 import numpy as np
-# import matplotlib as plt
-# import matplotlib.ticker as mticker
-# import seaborn as sns
+import matplotlib as plt
+import matplotlib.ticker as mticker
+import seaborn as sns
 import pandas as pd
 
 # Import our diameter data
@@ -21,24 +21,24 @@ beta_005=pd.DataFrame({'Normalized diameter':np.genfromtxt('cases_6112024/03_hb/
 beta_005['Method']='viscr=0.05'
 beta_005['Weight']=0.1
 
-# data=pd.concat([elvira,lvira,plicnet],ignore_index=True)
+data=pd.concat([newt,beta_05,beta_005],ignore_index=True)
 
-# # Set plotting style
-# #sns.set_theme()
-# sns.set_style("whitegrid")
-# sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+# Set plotting style
+sns.set_theme()
+sns.set_style("whitegrid")
+sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
 
-# # Histogram-style pdf
-# #g=sns.displot(data,bins=30,x='Normalized diameter',hue='Method',weights='Weight',log_scale=True,multiple='dodge',kde=True)
-# #g.set(yscale='log')
-# #g.set(ylabel='Scaled density')
-# #g.set(xbound=[1e-4,1])
-# #g.set(ybound=[1e-1,1e3])
+# Histogram-style pdf
+g=sns.displot(data,bins=30,x='Normalized diameter',hue='Method',weights='Weight',log_scale=True,multiple='dodge',kde=True)
+g.set(yscale='log')
+g.set(ylabel='Scaled density')
+g.set(xbound=[0.026,1])
+g.set(ybound=[1e-1,1e3])
 
-# # Log-log fitted pdf
+# Log-log fitted pdf
 # g=sns.kdeplot(data,x='Normalized diameter',hue='Method',weights='Weight',log_scale=[True,True],common_norm=True,bw_adjust=0.8,cut=0)
 # g.set(ylabel='Scaled density')
 # g.set(xbound=[1e-4,1])
-# g.set(ybound=[1e-4,1e0])
+# g.set(ybound=[1e-4,2])
 
-# plt.pyplot.show()
+plt.pyplot.show()
