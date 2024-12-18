1c4aa53b73de615555c9c766e65d41fc91155c91
jvg723 <jvg36@cornell.edu>
2024-12-18 22:46:19 +0000

    this method seems to be working for transporting vf

--- examples/simplex/src/vfs_class.f90
+++ examples/simplex/src/vfs_class.f90
@@ -1339,13 +1339,13 @@ contains
          Gvolnew=Gvolold+Gvolinc
          ! Compute new liquid volume fraction
          ! Conservative form
-         this%VF(i,j,k)=Lvolnew/(this%cfg%vol(i,j,k))
+         ! this%VF(i,j,k)=Lvolnew/(this%cfg%vol(i,j,k))
          
          ! Version 1
          ! this%VF(i,j,k)=Lvolnew/(this%cfg%vol(i,j,k))
          
          ! Version 2
-         ! this%VF(i,j,k)=Lvolnew/(this%cfg%vol(i,j,k)*(1.0_WP-div(i,j,k)*dt))
+         this%VF(i,j,k)=Lvolnew/(this%cfg%vol(i,j,k)*(1.0_WP-div(i,j,k)*dt))
 
          ! Only work on higher order moments if VF is in [VFlo,VFhi]
          if (this%VF(i,j,k).lt.VFlo) then
@@ -1369,11 +1369,11 @@ contains
             ! this%Gbary(:,i,j,k)=([this%cfg%xm(i),this%cfg%ym(j),this%cfg%zm(k)]-this%VF(i,j,k)*this%Lbary(:,i,j,k))/(1-this%VF(i,j,k))
 
             ! Another possible approach
-            ! if (this%VF(i,j,k).gt.0.9_WP) then
-            !    this%Gbary(:,i,j,k)= [this%cfg%xm(i),this%cfg%ym(j),this%cfg%zm(k)]
-            ! else 
-            !    this%Gbary(:,i,j,k)=([this%cfg%xm(i),this%cfg%ym(j),this%cfg%zm(k)]-this%VF(i,j,k)*this%Lbary(:,i,j,k))/(1-this%VF(i,j,k))
-            ! end if
+            if (this%VF(i,j,k).gt.0.9_WP) then
+               this%Gbary(:,i,j,k)= [this%cfg%xm(i),this%cfg%ym(j),this%cfg%zm(k)]
+            else 
+               this%Gbary(:,i,j,k)=([this%cfg%xm(i),this%cfg%ym(j),this%cfg%zm(k)]-this%VF(i,j,k)*this%Lbary(:,i,j,k))/(1-this%VF(i,j,k))
+            end if
          end if
       end do
 
