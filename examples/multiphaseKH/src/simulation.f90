970eff18249b1489f11d5899c7fef9390eb1449f
jvg723 <jvg36@cornell.edu>
2025-01-21 18:07:35 -0500

    add in missing division for height array

--- examples/multiphaseKH/src/simulation.f90
+++ examples/multiphaseKH/src/simulation.f90
@@ -91,8 +91,7 @@ contains
          end do
       end do
       ! All-reduce the data
-      call MPI_ALLREDUCE(my_height,height,vf%cfg%nx,MPI_REAL_WP,MPI_SUM,vf%cfg%comm,ierr)
-      height=height-Lyl
+      call MPI_ALLREDUCE(my_height,height,vf%cfg%nx,MPI_REAL_WP,MPI_SUM,vf%cfg%comm,ierr); height=height/real(vf%cfg%nx*vf%cfg%nz,WP)
       ! If root, print it out
       if (vf%cfg%amRoot) then
          ! call execute_command_line('mkdir -p stats')
