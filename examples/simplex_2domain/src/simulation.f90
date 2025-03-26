8049bdc8bdb7b8f5a3ee42d14f44e7e80b38791f
jvg723 <jvg36@cornell.edu>
2025-03-26 15:55:15 -0400

    switch or to and in coupling allows for the jet to be passed cleanly into the atomzation domain

--- examples/simplex_2domain/src/simulation.f90
+++ examples/simplex_2domain/src/simulation.f90
@@ -68,77 +68,77 @@ contains
       implicit none
 
 
-      ! Simplex drives overall time integration
-      do while (.not.spx%time%done())
-         
-         call spx%step()
-
-      end do
-      
       ! ! Simplex drives overall time integration
-      ! do while (.not.atomization%time%done())
+      ! do while (.not.spx%time%done())
          
-      !    ! ! Advance simplex simulation until it's caught up
-      !    ! do while (spx%time%t.le.atomization%time%t)
-      !    !    call spx%step()
-      !    ! end do
-
-      !    ! ! Handle coupling velocity between simplex and atomization
-      !    ! coupling_velocity_s2a: block
-      !    !    use tpns_class, only: bcond
-      !    !    integer :: n,i,j,k
-      !    !    type(bcond), pointer :: mybc
-      !    !    ! Exchange data using cpl12x/y/z couplers
-      !    !    call xcpl_s2a%push(spx%fs%U);   call xcpl_s2a%transfer();  call xcpl_s2a%pull(atomization%resU)
-      !    !    call ycpl_s2a%push(spx%fs%V);   call ycpl_s2a%transfer();  call ycpl_s2a%pull(atomization%resV)
-      !    !    call zcpl_s2a%push(spx%fs%W);   call zcpl_s2a%transfer();  call zcpl_s2a%pull(atomization%resW)
-      !    !    call atomization%fs%get_bcond('inlets',mybc)
-      !    !    do n=1,mybc%itr%no_
-      !    !       i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-      !    !       atomization%fs%U(i  ,j,k)=atomization%resU(i  ,j,k)*sum(atomization%fs%itpr_x(:,i  ,j,k)*atomization%cfg%VF(i-1:i,    j,    k))
-      !    !       atomization%fs%V(i-1,j,k)=atomization%resV(i-1,j,k)*sum(atomization%fs%itpr_y(:,i-1,j,k)*atomization%cfg%VF(i-1  ,j-1:j,    k))
-      !    !       atomization%fs%W(i-1,j,k)=atomization%resW(i-1,j,k)*sum(atomization%fs%itpr_z(:,i-1,j,k)*atomization%cfg%VF(i-1  ,j    ,k-1:k))
-      !    !    end do
-      !    ! end block coupling_velocity_s2a
-
-
-      !    ! ! Handle coupling VOF between simplex and atomization
-      !    ! coupling_vof_s2a: block
-      !    !    integer :: i,j,k
-      !    !    ! Exchange data using cell center coupler
-      !    !    atomization%tempVF=0.0_WP
-      !    !    call vfcpl_s2a%push(spx%vf%VF); call vfcpl_s2a%transfer(); call vfcpl_s2a%pull(atomization%tempVF)
-      !    !    ! Exchange VOF based upon x/y/z position
-      !    !    do k=atomization%vf%cfg%kmino_,atomization%vf%cfg%kmaxo_
-      !    !       do j=atomization%vf%cfg%jmino_,atomization%vf%cfg%jmaxo_
-      !    !          do i=atomization%vf%cfg%imino_,atomization%vf%cfg%imaxo_
-      !    !             if (atomization%vf%cfg%xm(i).ge.atomization%vfcouple_xmin.or.atomization%vf%cfg%xm(i).le.atomization%vfcouple_xmax) then
-      !    !             ! if ((atomization%vf%cfg%xm(i).ge.atomization%vfcouple_xmin.or.atomization%vf%cfg%xm(i).le.atomization%vfcouple_xmax).and.&
-      !    !             ! &   (atomization%vf%cfg%ym(j).ge.atomization%vfcouple_ymin.or.atomization%vf%cfg%ym(j).le.atomization%vfcouple_ymax).and.&
-      !    !             ! &   (atomization%vf%cfg%zm(k).ge.atomization%vfcouple_zmin.or.atomization%vf%cfg%zm(k).le.atomization%vfcouple_zmax)) then
-      !    !                atomization%vf%VF(i,j,k)=atomization%tempVF(i,j,k)
-      !    !             end if
-      !    !          end do
-      !    !       end do
-      !    !    end do
-      !    !    !> sync arrays
-      !    !    call atomization%cfg%sync(atomization%vf%VF)
-      !    !    ! call atomization%vf%advect_interface(dt=atomization%time%dt,U=atomization%fs%U,V=atomization%fs%V,W=atomization%fs%W)
-      !    !    ! call atomization%vf%reset_moments()
-      !    !    ! call atomization%vf%sync_and_clean_barycenters()   -> causes blow up? yes.... 
-      !    !    call atomization%vf%update_band()
-      !    !    ! call atomization%vf%build_interface()
-      !    !    ! call atomization%vf%polygonalize_interface()
-      !    !    ! call atomization%vf%distance_from_polygon()
-      !    !    ! call atomization%vf%subcell_vol()
-      !    !    ! call atomization%vf%get_curvature()
-      !    !    ! call atomization%vf%reset_volume_moments()
-      !    ! end block coupling_vof_s2a
-      
-      !    ! ! Advance atomization simulation
-      !    ! call atomization%step()
+      !    call spx%step()
 
       ! end do
+      
+      ! Simplex drives overall time integration
+      do while (.not.atomization%time%done())
+         
+         ! Advance simplex simulation until it's caught up
+         do while (spx%time%t.le.atomization%time%t)
+            call spx%step()
+         end do
+
+         ! Handle coupling velocity between simplex and atomization
+         coupling_velocity_s2a: block
+            use tpns_class, only: bcond
+            integer :: n,i,j,k
+            type(bcond), pointer :: mybc
+            ! Exchange data using cpl12x/y/z couplers
+            call xcpl_s2a%push(spx%fs%U);   call xcpl_s2a%transfer();  call xcpl_s2a%pull(atomization%resU)
+            call ycpl_s2a%push(spx%fs%V);   call ycpl_s2a%transfer();  call ycpl_s2a%pull(atomization%resV)
+            call zcpl_s2a%push(spx%fs%W);   call zcpl_s2a%transfer();  call zcpl_s2a%pull(atomization%resW)
+            call atomization%fs%get_bcond('inlets',mybc)
+            do n=1,mybc%itr%no_
+               i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+               atomization%fs%U(i  ,j,k)=atomization%resU(i  ,j,k)*sum(atomization%fs%itpr_x(:,i  ,j,k)*atomization%cfg%VF(i-1:i,    j,    k))
+               atomization%fs%V(i-1,j,k)=atomization%resV(i-1,j,k)*sum(atomization%fs%itpr_y(:,i-1,j,k)*atomization%cfg%VF(i-1  ,j-1:j,    k))
+               atomization%fs%W(i-1,j,k)=atomization%resW(i-1,j,k)*sum(atomization%fs%itpr_z(:,i-1,j,k)*atomization%cfg%VF(i-1  ,j    ,k-1:k))
+            end do
+         end block coupling_velocity_s2a
+
+
+         ! Handle coupling VOF between simplex and atomization
+         coupling_vof_s2a: block
+            integer :: i,j,k
+            ! Exchange data using cell center coupler
+            atomization%tempVF=0.0_WP
+            call vfcpl_s2a%push(spx%vf%VF); call vfcpl_s2a%transfer(); call vfcpl_s2a%pull(atomization%tempVF)
+            ! Exchange VOF based upon x/y/z position
+            do k=atomization%vf%cfg%kmino_,atomization%vf%cfg%kmaxo_
+               do j=atomization%vf%cfg%jmino_,atomization%vf%cfg%jmaxo_
+                  do i=atomization%vf%cfg%imino_,atomization%vf%cfg%imaxo_
+                     if (atomization%vf%cfg%xm(i).ge.atomization%vfcouple_xmin.and.atomization%vf%cfg%xm(i).le.atomization%vfcouple_xmax) then
+                     ! if ((atomization%vf%cfg%xm(i).ge.atomization%vfcouple_xmin.or.atomization%vf%cfg%xm(i).le.atomization%vfcouple_xmax).and.&
+                     ! &   (atomization%vf%cfg%ym(j).ge.atomization%vfcouple_ymin.or.atomization%vf%cfg%ym(j).le.atomization%vfcouple_ymax).and.&
+                     ! &   (atomization%vf%cfg%zm(k).ge.atomization%vfcouple_zmin.or.atomization%vf%cfg%zm(k).le.atomization%vfcouple_zmax)) then
+                        atomization%vf%VF(i,j,k)=atomization%tempVF(i,j,k)
+                     end if
+                  end do
+               end do
+            end do
+            !> sync arrays
+            call atomization%cfg%sync(atomization%vf%VF)
+            ! call atomization%vf%advect_interface(dt=atomization%time%dt,U=atomization%fs%U,V=atomization%fs%V,W=atomization%fs%W)
+            ! call atomization%vf%reset_moments()
+            ! call atomization%vf%sync_and_clean_barycenters()   -> causes blow up? yes.... 
+            ! call atomization%vf%update_band()
+            ! call atomization%vf%build_interface()
+            ! call atomization%vf%polygonalize_interface()
+            ! call atomization%vf%distance_from_polygon()
+            ! call atomization%vf%subcell_vol()
+            ! call atomization%vf%get_curvature()
+            ! call atomization%vf%reset_volume_moments()
+         end block coupling_vof_s2a
+      
+         ! Advance atomization simulation
+         call atomization%step()
+
+      end do
       
    end subroutine simulation_run
    
